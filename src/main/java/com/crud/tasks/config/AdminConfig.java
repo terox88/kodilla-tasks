359719f36ced44e8bcafb31b4ed8e594ce9e3616
Michal <terox@wp.pl>
2023-08-17 20:48:30 +0200

    32.2 Variables

--- src/main/java/com/crud/tasks/config/AdminConfig.java
+++ src/main/java/com/crud/tasks/config/AdminConfig.java
@@ -6,6 +6,9 @@ import org.springframework.stereotype.Component;
 @Component
 @Getter
 public class AdminConfig {
+@Value("${admin.name}")
+    private String adminName;
 @Value("${admin.mail}")
     private String adminMail;
+
 }
---
+++ src/main/java/com/crud/tasks/config/CompanyConfig.java
@@ -0,0 +1,20 @@
+package com.crud.tasks.config;
+
+import lombok.Getter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
+@Getter
+public class CompanyConfig {
+    @Value("${info.company.name}")
+    private String name;
+    @Value("${info.company.goal}")
+    private String goal;
+    @Value("${info.company.email}")
+    private String email;
+    @Value("${info.company.phone}")
+    private String phone;
+
+
+}
